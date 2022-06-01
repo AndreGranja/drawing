c8c11d34e24539d21ac3d95ef8a597e194a65b19
AndreGranja <andregranja1@gmail.com>
2022-06-01 10:57:00 +0100

    My commit

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from drawing import shapes, colors
-shapes.oval()
-colors.red()
+from drawing import oval, red
+from drawing.advanced.tools import pencil, text, eraser
+oval()
+red()
+
+pencil()
+text()
+eraser()
